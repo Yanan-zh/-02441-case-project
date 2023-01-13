2e5b44b57e5c2184d8324df0dd2db9cd174e74ab
astridlh <astridlouiseharsaae@gmail.com>
2023-01-13 11:38:48 +0100

    final model !!!

--- Case2/scripts/analysis.R
+++ Case2/scripts/analysis.R
@@ -15,19 +15,6 @@ data$weekday <-weekdays(as.Date(data$date))
 data$weekend <- as.factor((data$weekday == "Saturday" | data$weekday == "Sunday"))
 data$consumption <- data$consumption +0.00001
 
-summary(data)
-
-# fix dates ---------------------------------------------------------------
-
-uniqueDates <- unique(data$date)
-
-ordering <- seq(1,118,1)
-
-# initial visualization ---------------------------------------------------
-
-pairs(data)
-
-
 # initial model -----------------------------------------------------
 
 model_initial <- lm(consumption~ ID+ tempDiff,data)
@@ -58,44 +45,144 @@ plot(model_wInter, col = data$weekend)
 
 # model with weekend ------------------------------------------------------
 
-par(mfrow=c(1,1))
+# so we did this entire model, and according to AIC we keep being told to keep parameters, 
+# going through it again with BIC, as it is more conservative, and we have an abundance of variables.
+
+par(mfrow=c(1,2))
 plot(data$consumption~data$tempDiff, col = data$weekend)
+plot(data$consumption~data$weekend)
+
+# we are not able to see any larger difference in means, but we do a quick test anyways
+
+wilcox.test(data[data$weekend==TRUE,]$consumption,data[data$weekend==FALSE,]$consumption)
+
+# is does not look it, but it is significant, so we add it
+
+model_wWeekend <- update(model_wInter,. ~ . *weekend)
 
 
-# model with the weekend
+# now we check to see if we should remove anything
 
-model_wWeekend <-lm(consumption~ weekend +ID+ tempDiff +ID:tempDiff + ID:weekend + weekend:tempDiff,data)
-drop1(model_wWeekend, test = "F")
+drop1(model_wWeekend, test = "F", k = log(length(data)))
+
+#so according to the BIC we remove ID:tempDiff:weekend
+
+model_wWeekend <- update(model_wWeekend,. ~ . -ID:tempDiff:weekend)
+
+drop1(model_wWeekend, test = "F", k = log(length(data)))
+
+# no more dropping! We check Anova
+
+Anova(model_wWeekend)
+
+#alrighty we like the look of that!
 
 par(mfrow=c(2,2))
 plot(model_wWeekend, col = data$ID)
 
-
-
 # model with weather variables --------------------------------------------
 
+
+# by brain power - cold wind probably means we use more energy to heat buildings?
+
+#make a better plot !!!
+par(mfrow=c(1,1))
 plot(data$consumption ~data$wind_spd, col = data$ID)
 
-model_wWind <- lm(consumption~ wind_spd +weekend +ID+ tempDiff +ID:tempDiff + ID:weekend + weekend:tempDiff +wind_spd:tempDiff+wind_spd:ID+wind_spd:weekend,data)
-drop1(model_wWind, test = "F")
+# it maybe looks like there are different slopes for the various buildings, we add interactions
 
-model_wWind <- update(model_wWind,. ~ . - wind_spd:weekend)
-drop1(model_wWind, test = "F")
+model_wWind <-update(model_wWeekend,. ~ . *wind_spd)
+
+drop1(model_wWind, test = "F", k = log(length(data)))
+
+# makes sense, ID:weekend:wind_spd is very important
+
+model_wWind <- update(model_wWind,. ~ . - ID:weekend:wind_spd)
+
+drop1(model_wWind, test = "F", k = log(length(data)))
+
+# remove ID:tempDiff:wind_spd
+
+model_wWind <- update(model_wWind,. ~ . - ID:tempDiff:wind_spd)
+
+drop1(model_wWind, test = "F", k = log(length(data)))
+
+# remove tempDiff:weekend:wind_spd
+
+model_wWind <- update(model_wWind,. ~ . - tempDiff:weekend:wind_spd)
+
+drop1(model_wWind, test = "F", k = log(length(data)))
 
 
-# add some wind direction based on some plots
-par(mfrow=c(1,1))
+par(mfrow=c(1,2))
 plot(data$consumption ~data$wind_spd, col = data$dir)
+plot(data$consumption ~data$tempDiff, col = data$dir)
 
-model_wDir <- update(model_wWind,. ~ . +dir)
-model_wDir <- update(model_wWind,. ~ . +dir:wind_spd)
+
+#looks like we got some interaction?
+
+model_wWind <- update(model_wWind,. ~ . +dir + wind_spd:dir + wind_spd:dir:ID +wind_spd:dir:tempDiff)
+
+drop1(model_wWind, test = "F", k = log(length(data)))
+
+# based on this, remove ID:wind_spd:dir
+
+model_wWind <- update(model_wWind,. ~ . - ID:wind_spd:dir)
+
+drop1(model_wWind, test = "F", k = log(length(data)))
+
+#acording to this we do not want to remove anything else
 
 par(mfrow=c(2,2))
-plot(model_wDir)
+plot(model_wWind)
 
-model_final <- lm(consumption~ wind_spd +weekend +ID+ tempDiff +ID:tempDiff + ID:weekend + weekend:tempDiff +wind_spd:tempDiff+wind_spd:ID+dir+dir:wind_spd,data)
+
+#we do some analysis on the model
+Anova(model_wWind)
+
+#shoes we have some multicolinearity, lets track that down 
+alias(model_wWind)
+
+# we try and remove tempDiff:wind_spd:dir
+model_wWind <- update(model_wWind,. ~ . - tempDiff:wind_spd:dir)
+
+Anova(model_wWind)
+
+# visual analysis 
 
 par(mfrow=c(2,2))
-plot(model_wDir)
+plot(model_wWind)
 
 
+
+# should we add more? -----------------------------------------------------
+
+# next thing that makes the most sense from the weather stuff is the humididity
+
+model_wHum <- update(model_wWind,. ~ . +hum)
+drop1(model_wHum, test = "F", k = log(length(data)))
+
+#according to the BIC, it makes sense to add ! we check for multicplinearity
+
+Anova(model_wHum)
+
+# we try and add some interactions
+
+model_wHum <- update(model_wHum,. ~ . +hum:wind_spd +hum:ID)
+drop1(model_wHum, test = "F", k = log(length(data)))
+
+# we keep these! maybe there are some buildings which have issues with windspeed and hum in certain buildings
+
+model_wHum <- update(model_wHum,. ~ . +hum:wind_spd:ID)
+drop1(model_wHum, test = "F", k = log(length(data)))
+
+# no 
+
+model_wHum <- update(model_wHum,. ~ . -hum:wind_spd:ID)
+drop1(model_wHum, test = "F", k = log(length(data)))
+
+
+
+# final model -------------------------------------------------------------
+
+final_model <- model_wHum
